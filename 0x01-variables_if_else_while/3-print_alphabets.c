5f36fee3b840756b6f372de30b2cc387f45d2b4d
“Chiamaka <vera4amaka@yahoo.com>
2022-09-12 16:22:23 -0700

    adds caps alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description:  prints alphabets in lowercase,
+ *and in uppercase.
+ *Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
