6810578314c06e217874caef09773748768b274d
“Chiamaka <vera4amaka@yahoo.com>
2022-09-12 17:48:21 -0700

    adds more files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *Description: prints all the numbers of base 16 in lowercase
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	char c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
