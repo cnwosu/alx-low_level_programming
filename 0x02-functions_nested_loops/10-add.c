9db60bad9c7849988846d672fe691df4f503b2d9
“Chiamaka <vera4amaka@yahoo.com>
2022-09-14 15:16:31 -0700

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *add - Entry point
+ *
+ *Description: Adds two numbers
+ *
+ *Return: returns int (c)
+ *
+ *@d: The int param
+ *
+ *@g: The int param
+ */
+
+int add(int d, int g)
+{
+	int c;
+
+	c = d + g;
+
+	return (c);
+}
