8db14073cb0bb3749825fd51d0bce78eb66d9825
“Chiamaka <vera4amaka@yahoo.com>
2022-09-14 08:01:57 -0700

    adds repeated alphabets up to 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry function of this code
+ *
+ *description: prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int ten_x = 10;
+	char alpha;
+
+	while (ten_x < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+
+		}
+
+		ten_x++;
+		_putchar('\n');
+	}
+}
