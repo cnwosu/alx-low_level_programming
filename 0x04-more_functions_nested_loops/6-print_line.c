de375d28896dbc4ab431635b837fa3347ee46570
“Chiamaka <vera4amaka@yahoo.com>
2022-09-16 17:48:33 -0700

    adds functions to draw straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line - Draws a straight line in the terminal
+ *
+ *@n: numbers of time the character should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
