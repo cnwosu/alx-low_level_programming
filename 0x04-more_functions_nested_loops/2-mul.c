69f4a7094db9021395a4a403a3ff0e9f56e720d4
“Chiamaka <vera4amaka@yahoo.com>
2022-09-16 17:07:30 -0700

    adds multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *mul - Function mul declaration
+ *
+ *Description: Multiply two integers
+ *
+ *@a: Param an integer
+ *
+ *@b: Param 2 an integer
+ *
+ *Return: return product
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
