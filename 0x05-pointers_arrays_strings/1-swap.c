33ab26b3552d408f09217722d5cdf8f06a4a0c55
“Chiamaka <vera4amaka@yahoo.com>
2022-09-19 16:58:43 -0700

    adds swap file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - function to swap the value of the parameters
+ *
+ *@a: param 1
+ *@b: param 2
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
