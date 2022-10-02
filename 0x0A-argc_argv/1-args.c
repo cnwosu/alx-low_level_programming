fe0e0045039c6fe966994ed2cb15854ee78aecaa
“Chiamaka <vera4amaka@yahoo.com>
2022-10-02 15:31:17 -0700

    bug fixes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - print the output od argc (number of arguments)
+ * @argc: keeps count of number of arguments
+ * @argv: stores records of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argv; /*This command is used to Ignore argv*/
