91bcad8290e91b17accfb157c7296d5c28093324
“Chiamaka <vera4amaka@yahoo.com>
2022-10-11 02:24:38 -0700

    attempt'

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
+
+void _print(char *str, int l)
+{
+}
