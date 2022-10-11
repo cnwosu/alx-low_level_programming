dc5498978f1797f7729d5268da537729787ab2ca
“Chiamaka <vera4amaka@yahoo.com>
2022-10-11 11:26:38 -0700

    structure definition for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 typedef struct dog dog_t;
 
@@ -22,4 +22,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*STRTYP*/
+#endif /*DOG_H*/
